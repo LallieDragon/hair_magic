2c00e1fb3cb607746f8377a2177703a9e04ca8d0
LallieDragon <c.simecheck89@yahoo.com>
2019-09-23 13:05:52 -0500

    form works with formspree.io

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,62 +1,45 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
 
-class FormPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
+const FormPage = (props) => {
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol>
+          <form id="contact-form" action={`https://formspree.io/${props.email}`} method="POST">
+            <p className="h4 text-center mb-4">Write us to book an appointment</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" className="form-control" />
 
-  render () {
-    return (
-      <MDBContainer>
-        <MDBRow>
-          <MDBCol>
-            <form id="contact-form">
-              <p className="h4 text-center mb-4">Write us to book an appointment</p>
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" className="form-control" onChange={this.handleChange} />
+            <br />
 
-              <br />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" className="form-control" />
 
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" className="form-control" onChange={this.handleChange} />
+            <br />
 
-              <br />
+            <label htmlFor="subject">Subject</label>
+            <input type="text" name="subject" className="form-control" />
 
-              <label htmlFor="subject">Subject</label>
-              <input type="text" name="subject" className="form-control" onChange={this.handleChange} />
+            <br />
 
-              <br />
+            <label htmlFor="message">Message</label>
+            <textarea type="text" message="message" className="form-control" rows="3" />
 
-              <label htmlFor="message">Message</label>
-              <textarea type="text" message="message" className="form-control" rows="3" onChange={this.handleChange} />
+            <div className="text-center mt-4">
+              <MDBBtn style={{ marginBottom: "2rem" }} color="cyan lighten-3" outline type="submit" value="Send">
+                Send
+                <MDBIcon far icon="paper-plane" className="ml-2" />
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
 
-              <div className="text-center mt-4">
-                <MDBBtn style={{ marginBottom: "2rem" }} color="cyan lighten-3" outline type="submit" value="Send">
-                  Send
-                  <MDBIcon far icon="paper-plane" className="ml-2" />
-                </MDBBtn>
-              </div>
-            </form>
-          </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
 
-        </MDBRow>
-      </MDBContainer>
-    );
-  };
-}
 
 export default FormPage;
--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { MDBCol, MDBRow } from 'mdbreact';
 import { initClient } from '../contentfulClient';
-import { social, location } from '../contentful/Keys';
-// import ContactForm from '../components/ContactForm';
+import { social } from '../contentful/Keys';
+import ContactForm from '../components/ContactForm';
 import Spinner from '../components/Spinner';
 import SocialMedia from '../components/SocialMedia';
 
@@ -10,12 +10,9 @@ class ContactContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      socialContent: null,
-      locationContent: null
+      socialContent: null
     }
-
     this.getSocialContent = this.getSocialContent.bind(this);
-    this.getLocationContent = this.getLocationContent.bind(this);
   }
 
   getSocialContent = (key) => {
@@ -28,31 +25,18 @@ class ContactContainer extends React.Component {
     .catch('Error: ' + console.error)
   }
 
-  getLocationContent = (key) => {
-    let client = initClient()
-
-    client.getEntry(key)
-    .then((entry) => this.setState({
-      locationContent: entry.fields
-     }))
-    .catch('Error: ' + console.error)
-  }
-
   render() {
     if (this.state.socialContent === null) {
       this.getSocialContent(social)
       return <Spinner />
-    } else if (this.state.locationContent === null){
-      this.getLocationContent(location)
-      return <Spinner />
     }
 
-    // <MDBCol md="6">
-    //   <ContactForm />
-    // </MDBCol>
     return(
       <MDBRow className="contact-container">
-        <MDBCol md="12">
+        <MDBCol md="6">
+          <ContactForm email={this.state.socialContent.links[2]}/>
+        </MDBCol>
+        <MDBCol md="6">
           <SocialMedia content={this.state.socialContent.links}/>
         </MDBCol>
       </MDBRow>
